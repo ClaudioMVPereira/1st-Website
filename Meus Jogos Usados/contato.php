eaec16db1cba0dff5dce93fc555bd4e95a32d085
ClaudioMVPereira <94339351+ClaudioMVPereira@users.noreply.github.com>
2021-11-30 20:08:06 -0300

    Update contato.php

--- Meus Jogos Usados/contato.php
+++ Meus Jogos Usados/contato.php
@@ -1,19 +1,20 @@
 <?php 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $mensagem = $_POST['mensagem'];
+  <?php 
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $mensCoagem = $_POST['mensagem'];
 
-    //conexao com Database
-    $conn = new mysqli('localhost', 'root', '', 'Contato');
-    if($conn->connect_error){
-        die('Conexão Falhou : '.$conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into registration(nome, email, mensagem) 
-        values (?, ?, ?)");
-        $stmt->bind_param("sss", $name, $email, $mensagem);
-        $stmt->execute();
-        echo "Obrigado pelo contato, retornaremos assim que possível";
-        $stmt->close();
-        $conn->close();
-    }
+  //conexao com Database
+  $conn = new mysqli('localhost','root','','Contato');
+  if($conn->connect_error){
+      die('Conexão Falhou : '.$conn->connect_error);
+  }else{
+      $stmt = $conn->prepare("insert into registration(nome, email, mensagem) 
+      values (?, ?, ?)");
+      $stmt->bind_param("sss", $name, $email, $mensagem);
+      $execval = $stmt->execute();
+      echo "Obrigado pelo contato, retornaremos assim que possível";
+      $stmt->close();
+      $conn->close();
+  }
 ?>
